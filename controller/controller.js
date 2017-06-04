0bb1b416224b2237fbbca72e3dc59a0ff75f0555
vignesh-sankar <vigneshssankar@gmail.com>
2017-06-03 17:35:37 -0700

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -15,15 +15,32 @@ exports.login = function(req, res){
 	var queryDB = "SELECT * from LoginDetails where email='"+email+"' and password='"+pass+"'";
 	console.log(queryDB);
 	connection.query("SELECT * from LoginDetails where email='"+email+"' and password='"+pass+"'", function (err, rows, fields) {
-	  	if (err){ console.log(err); }
+	  	if (err){ console.log(err);}
 	  	// console.log(err);
 	  	// console.log('The solution is: ', rows[0].name);
 		else{
 	  		console.log(rows[0]);
+			if(rows.length == 0) res.redirect('/');
+			else res.redirect('/dashboard');
 			//res.send(rows[0].name);
-			return;
+			//return;
 		}
 	});	
 	//connection.end();
+	
+};
+
+exports.create = function(req,res){
+	var email = req.body.email;
+	var pass = req.body.password;
+	var username=req.body.username;
+	var insertDB = "INSERT INTO LoginDetails (email,password,name) VALUES (\""+email+"\",\""+pass+"\",\""+username+"\")";
+	console.log(insertDB);
+	connection.query(insertDB,function(err,result){
+		if(err) throw err
+		//console.log("Login details inserted");
+		else {
+		res.redirect('/dashboard');}
+	});
 	
 };
